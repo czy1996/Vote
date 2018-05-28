fd5492ddfd8ffc4d196fc51a539c3185cc436cdf
helion <czy1996@users.noreply.github.com>
2018-05-28 20:19:57 +0800

    起止日期 picker

--- frontend/src/components/page/CreateVote.vue
+++ frontend/src/components/page/CreateVote.vue
@@ -32,22 +32,81 @@
                 />
               </v-flex>
             </v-layout>
-            <v-layout row>
+            <v-layout id="layout-time-start" row>
               <v-flex xs4>
                 <v-subheader>开始时间</v-subheader>
               </v-flex>
-              <v-flex xs8>
-
+              <v-flex xs3>
+                <v-menu id="date-picker-start"
+                        ref="menuDateStart"
+                        :close-on-content-click="false"
+                        v-model="menuDateStart"
+                        :nudge-right="40"
+                        :return-value.sync="dateStart"
+                        lazy
+                        transition="scale-transition"
+                        offset-y
+                        full-width
+                        min-width="290px"
+                >
+                  <v-text-field
+                    slot="activator"
+                    v-model="dateStart"
+                    label="开始日期"
+                    prepend-icon="event"
+                    readonly
+                  />
+                  <v-date-picker
+                    v-model="dateStart"
+                    no-title
+                    scrollable
+                    v-bind:min="today"
+                  >
+                    <v-spacer/>
+                    <v-btn flat color="primary" @click="menuDateStart = false">Cancel</v-btn>
+                    <v-btn flat color="primary" @click="$refs.menuDateStart.save(dateStart)">OK</v-btn>
+                  </v-date-picker>
+                </v-menu>
               </v-flex>
-            </v-layout><v-layout row>
+            </v-layout>
+            <v-layout id="layout-time-end" row>
               <v-flex xs4>
                 <v-subheader>结束时间</v-subheader>
               </v-flex>
-              <v-flex xs8>
-
+              <v-flex xs3>
+                <v-menu id="date-picker-end"
+                        ref="menuDateEnd"
+                        :close-on-content-click="false"
+                        v-model="menuDateEnd"
+                        :nudge-right="40"
+                        :return-value.sync="dateEnd"
+                        lazy
+                        transition="scale-transition"
+                        offset-y
+                        full-width
+                        min-width="290px"
+                >
+                  <v-text-field
+                    slot="activator"
+                    v-model="dateEnd"
+                    label="结束日期"
+                    prepend-icon="event"
+                    readonly
+                  />
+                  <v-date-picker
+                    v-model="dateEnd"
+                    no-title
+                    scrollable
+                    v-bind:min="today"
+                  >
+                    <v-spacer/>
+                    <v-btn flat color="primary" @click="menuDateEnd = false">Cancel</v-btn>
+                    <v-btn flat color="primary" @click="$refs.menuDateEnd.save(dateEnd)">OK</v-btn>
+                  </v-date-picker>
+                </v-menu>
               </v-flex>
             </v-layout>
-            <v-layout row>
+            <v-layout id="layout-number-of-options" row>
               <v-flex xs4>
                 <v-subheader>选项数量</v-subheader>
               </v-flex>
@@ -61,7 +120,7 @@
                 />
               </v-flex>
             </v-layout>
-            <v-layout row>
+            <v-layout id="layout-options" row>
               <v-flex xs4>
                 <v-subheader>选项</v-subheader>
               </v-flex>
@@ -96,6 +155,7 @@
 
 <script>
   import vote from '../../api/vote'
+
   export default {
     name: 'create-vote',
     data () {
@@ -104,6 +164,10 @@
         title: '',
         description: '',
         options_helper: [],
+        dateStart: null,
+        dateEnd: null,
+        menuDateStart: false,
+        menuDateEnd: false,
       }
     },
     methods: {
@@ -133,6 +197,16 @@
         }
         this.options_helper = result
         return result
+      },
+      today () {
+        let date = new Date()
+        let Y = date.getFullYear()
+        let M = date.getMonth() + 1
+        M = M < 10 ? '0' + M : M // 不够两位补充0
+        let D = date.getDate()
+        D = D < 10 ? '0' + D : D
+        this.$log('today', Y + '-' + M + '-' + D)
+        return Y + '-' + M + '-' + D
       },
     },
   }
